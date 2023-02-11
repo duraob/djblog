38c6e8e2b728c8e92cab47137a48a9f1db44ea21
duraob <duraoben@gmail.com>
2023-02-11 18:38:42 -0500

    dynamic views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
+def post_list(request):
+    ## All Posts published prior to now, sorted by desc
+    posts = Post.objects.filter(published__date__lte=timezone.now().order_by('published_date'))
+    return render(request, 'blog/post_list.html', {'posts':posts})
