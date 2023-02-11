29bc8e30aefea0c0d91125e429e1f53789629ed3
duraob <duraoben@gmail.com>
2023-02-11 18:42:08 -0500

    fixed chained method

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,5 +5,5 @@ from django.utils import timezone
 # Create your views here.
 def post_list(request):
     ## All Posts published prior to now, sorted by desc
-    posts = Post.objects.filter(published__date__lte=timezone.now().order_by('published_date'))
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
