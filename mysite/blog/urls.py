00777e7d2e3fcdd9fb5eed534f4b975368c898a8
duraob <duraoben@gmail.com>
2023-02-12 09:43:34 -0500

    page_detail template, view, url

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    ## Create a path to link to post by its pk, use the post_detail view, name it post_detail
+    path('post/<int:pk>/', views.post_detail, name='post_deatil')
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -7,3 +7,9 @@ def post_list(request):
     ## All Posts published prior to now, sorted by desc
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    ## From the request get the pk & search for that obj in posts table, return dtls render view
+    ## if obj doesnt exist return the 404 page
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post' : post})
