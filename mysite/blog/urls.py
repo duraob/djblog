4071493990f49da20b5c1b311b10a2f445691a8f
duraob <duraoben@gmail.com>
2023-02-12 09:47:47 -0500

    fixed spelling err that caused not found bug

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     ## Create a path to link to post by its pk, use the post_detail view, name it post_detail
-    path('post/<int:pk>/', views.post_detail, name='post_deatil')
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
