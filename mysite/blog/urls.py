53d5f2e3e37150baac12df36ecfe18683cb7b9cb
duraob <duraoben@gmail.com>
2023-02-12 10:27:24 -0500

    added UPDATE posts: template, view, url

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     ## Create a path to link to post by its pk, use the post_detail view, name it post_detail
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     ## Create a path to create a new post
-    path('post/new/', views.post_new, name='post_new')
+    path('post/new/', views.post_new, name='post_new'),
+    ## Create a path to update an existing post
+    path('post/<int:pk>/edit', views.post_edit, name='post_edit')
 ]
